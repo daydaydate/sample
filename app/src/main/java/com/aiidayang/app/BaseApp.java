fc06ba878e32fe067b2b0dcb856c01334ad1cbeb
daydaydate <929048200@qq.com>
2020-05-27 22:59:09 +0800

    解决冲突

--- app/src/main/java/com/aiidayang/app/BaseApp.java
+++ app/src/main/java/com/aiidayang/app/BaseApp.java
@@ -23,7 +23,7 @@ public class BaseApp extends Application {
         applicationContext = getApplicationContext();
         threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-        “制造冲突”
+        //“解决冲突”
         initNet();
         initHyphenate();
     }
